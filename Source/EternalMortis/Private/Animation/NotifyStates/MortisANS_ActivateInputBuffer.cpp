8a93a9238f0e3a934dc636597138a9946ec2bd1b
Threemarine <84086266+tldbs0420@users.noreply.github.com>
2026-03-17 14:14:10 +0900

    [PR] Added Input Buffer And Camera Zoom (#29)
    * [MODIFY] Added Dynamic Asset Tag To Player Attack Effect
    
    플레이어 공격 이펙트 생성 시 공격 종류를 Dynamic Tag로 포함하도록 변경
    
    - MortisPlayerGameplayAbility.h의 MakePlayerBaseDamageUpdateEffectSpecHandle 함수에 태그 인자 추가 및 GE Spec Handle에 할당
    - 데미지 계산 시 저항 선택에 Magic이 정상적이지 않던 문제 해결
    
    ==============================
    
    Changed to add attack type using dynamic tag when creating player attack effect
    
    - MakePlayerBaseDamageUpdateEffectSpecHandle function of MortisPlayerGameplayAbility.h now has tag arg and assigns it at GE Spec Handle
    - Problem that Resist Selection On Magic Did Not Work Well Is Solved
    
    * [ADD] Added Input Buffer And Camera Zoom
    
    플레이어 공격 중 공격 입력, 스킬 입력, 구르기 입력을 기억하여 공격이 끝나면 다음 동작으로 이어짐.
    공격 중 항상 그렇진 않고, 중간 즈음부터 공격 마무리까지의 입력만 기억됨.
    여러 입력이 올 경우 마지막에 온 입력으로 행동을 진행함.
    
    Player의 Tick을 활성화하여 카메라 줌인/아웃이 생김
    세부 파라미터 조정은 BP_PlayerCharacter에서 가능
    
    - 입력버퍼 활성화 판별 bool과 버퍼로 처리할 어빌리티 태그를 플레이어 코드에 추가
    - 인풋 버퍼를 활성화해주는 ANS 생성
    - 모든 기본 공격 몽타주에 공격 판정 토글과 인풋 버퍼 활성화 ANS 추가
    - 플레이어 BP에 카메라값 조정
    - 공격 어빌리티 종료 후 인풋 버퍼 내용을 수행하도록 수정
    - (버그) 달리기 어빌리티의 Tag에 삭제된 Tag가 할당되어있던 문제 해결
    - 마우스 스크롤에 따른 Zoom In/Out 추가
    
    ==============================
    
    The player now remembers Attack, Skill, and Dodge inputs during an attack, and chains into the next action once the attack ends.
    These inputs are not buffered for the entire attack; only inputs entered from around the middle of the animation through the end of the attack are remembered.
    If multiple inputs are received, the most recent one takes priority.
    
    Player Tick was enabled to support camera zoom in and out.
    Detailed parameter tuning can be adjusted in BP_PlayerCharacter.
    
    - Added a bool in the player code to determine whether input buffering is active, along with gameplay ability tags to be handled through the buffer
    - Created an AnimNotifyState that enables the input buffer
    - Added both the hitbox toggle and input buffer activation AnimNotifyState to all basic attack montages
    - Adjusted camera-related values in the player Blueprint
    - Modified the attack ability so that buffered input is executed after the ability ends
    - (bug) Fixed a bug where the Sprint ability had been assigned a deleted tag
    - Added zoom in/out using the mouse scroll wheel

---
+++ Source/EternalMortis/Private/Animation/NotifyStates/MortisANS_ActivateInputBuffer.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/NotifyStates/MortisANS_ActivateInputBuffer.h"
+#include "Character/Player/MortisPlayerCharacter.h"
+
+void UMortisANS_ActivateInputBuffer::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+	ToggleInputBuffer(MeshComp, true);
+}
+
+void UMortisANS_ActivateInputBuffer::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+	ToggleInputBuffer(MeshComp, false);
+}
+
+void UMortisANS_ActivateInputBuffer::ToggleInputBuffer(USkeletalMeshComponent* MeshComp, bool bEnable)
+{
+	if (!MeshComp) return;
+
+	AActor* Owner = MeshComp->GetOwner();
+	if (!Owner) return;
+
+	if (AMortisPlayerCharacter* MortisPlayer = Cast<AMortisPlayerCharacter>(Owner))
+		MortisPlayer->SetBufferEnabled(bEnable);
+}
